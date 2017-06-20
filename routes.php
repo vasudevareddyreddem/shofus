138d3f510c1142a69c6a7366945cdc6e9fc390b4
raghava yaddanapudi <vasudareddy549@gmail.com>
2017-06-20 17:59:23 +0530

    testing
    testing

--- routes.php
+++ routes.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 |
 |	https://codeigniter.com/user_guide/general/routing.html
-|
+|
 | -------------------------------------------------------------------------
 | RESERVED ROUTES
 | -------------------------------------------------------------------------
