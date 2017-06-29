b22e56a9925eaea71afda9cbbaac445c03b2dfdd
vasu <vasudevareddy549@gmail.com>
2017-06-29 12:06:24 +0530

    search functionality purpose
    search functionality purpose

--- application/views/customer/search.php
+++ application/views/customer/search.php
@@ -4,9 +4,9 @@
 foreach($detail as $searhitems){  
  if($searhitems['yes']==0){  ?>
 	 
-	<li style="list-style-type:none;"><a  style="text-delection:none;" href=""><?php  echo $searhitems['subcategory_name'];  ?></a></li> 
+	<li style="list-style-type:none;"><a  style="text-delection:none;" href="<?php echo base_url('category/view/'.base64_encode($searhitems['subcategory_id'])); ?>"><?php  echo $searhitems['subcategory_name'];  ?></a></li> 
   <?php }else{ ?>
-	 <li style="list-style-type:none;"><a  style="text-delection:none;" href=""><?php  echo $searhitems['item_name'];  ?></a></li> 
+	 <li style="list-style-type:none;"><a  style="text-delection:none;" href="<?php echo base_url('category/productview/'.base64_encode($searhitems['item_id'])); ?>"><?php  echo $searhitems['item_name'];  ?></a></li> 
 
 	 
 <?php  } } ?>
