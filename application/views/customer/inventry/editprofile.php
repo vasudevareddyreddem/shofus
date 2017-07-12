36fa2d6a991f7b9533b680b8f3de65b8b905a87b
vasu <vasudevareddy549@gmail.com>
2017-07-12 20:25:59 +0530

    inventry management profile purpose
    inventry management profile purpose

--- application/views/customer/inventry/editprofile.php
+++ application/views/customer/inventry/editprofile.php
@@ -33,7 +33,7 @@
 					<span aria-hidden="true">&times;</span>
 					</button><?php echo $this->session->flashdata('errormsg');?></div>	
 					<?php endif; ?>
-                   <form id="editprofile" name="editprofile" method="post" action="<?php echo base_url('customer/updateprofilepost');?>" class="form-horizontal" enctype="multipart/form-data" role="form">
+                   <form id="editprofile" name="editprofile" method="post" action="<?php echo base_url('inventory/updateprofilepost');?>" class="form-horizontal" enctype="multipart/form-data" role="form">
                         <div class=" form-group">
                             <label class="control-label">First Name</label>
                             <input type="text" class="form-control" id="fname" name="fname" value="<?php echo $profile_details['cust_firstname']; ?>">
@@ -87,3 +87,81 @@
 	   </div>
 	   </div>
 </div>
+<script type="text/javascript">
+
+$(document).ready(function() {
+    $('#editprofile').bootstrapValidator({
+       
+        fields: {
+            fname: {
+              validators: {
+					notEmpty: {
+						message: 'First Name is required'
+					},
+                   regexp: {
+					regexp: /^[a-zA-Z0-9. ]+$/,
+					message: ' Last Name can only consist of alphanumaric, space and dot'
+					}
+                }
+            },
+			lname: {
+              validators: {
+					notEmpty: {
+						message: 'Last Name is required'
+					},
+                   regexp: {
+					regexp: /^[a-zA-Z0-9. ]+$/,
+					message: ' Last Name can only consist of alphanumaric, space and dot'
+					}
+                }
+            },
+			
+			email: {
+             validators: {
+					notEmpty: {
+						message: 'Email is required'
+					},
+					regexp: {
+					regexp: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+					message: 'Please enter a valid email address. For example johndoe@domain.com.'
+					}
+				}
+            },
+			mobile: {
+              validators: {
+					 notEmpty: {
+						message: 'Mobile Number is required'
+					},
+                    regexp: {
+					regexp:  /^[0-9]{10}$/,
+					message:'Mobile Number must be 10 digits'
+					}
+                }
+            },
+			
+			address1: {
+				validators: {
+					notEmpty: {
+						message: 'Address1 is required'
+					},
+					regexp: {
+					regexp:/^[ A-Za-z0-9_@.,/!;:}{@#&`~"\\|^?$*)(_+-]*$/,
+					message: 'Address1 wont allow <> [] = % '
+					}
+				
+				}
+			},
+			
+			profile: {
+				validators: {
+					  
+					regexp: {
+					regexp: /\.(jpe?g|png|gif)$/i,
+					message: 'Uploaded file is not a valid image. Only JPG, PNG and GIF files are allowed'
+					}
+				}
+			},
+        }
+    });
+});
+</script>
