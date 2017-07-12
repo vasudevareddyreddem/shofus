5a31260347946e6c870761796e40d2738713bf90
bayapucg <vasudevareddy549@gmail.com>
2017-07-12 13:25:45 +0530

    seller details
    page design

---
+++ application/views/customer/inventry/login.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>::CART IN HOUR::</title>
+    <link rel="icon" href="<?php echo base_url(); ?>assets/home/images/fav.ico">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/home/css/font-awesome.min.css">
+
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/home/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/home/css/style.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/dist/css/bootstrapValidator.css" />
+
+
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/home/css/default.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/home/css/component.css" />
+    <script src="<?php echo base_url(); ?>assets/home/js/jquery.js"></script>
+
+    <script src="<?php echo base_url(); ?>assets/home/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/dist/js/bootstrapValidator.js"></script>
+
+</head>
+<style>
+    .mar_t10per {
+        margin-top: 10%;
+    }
+    .form .form-control {
+        margin-bottom: 10px;
+    }
+</style>
+
+<body style="background-color:#f59171;">
+    <div class="container mar_t10per" style="position:relative" >
+        <div class="row ">
+		<div class="col-md-6 col-md-offset-3" style="background-color:#fff; border-radius:10px;padding:10px 0px; ">
+            <div class="col-md-12 " style="border-right:1px solid #ddd">
+			<div class="" style="padding:0px 15px;">
+			<h3 class="">Sign in</h3>
+			<hr>
+                <div id="loginbox" class="mainbox ">
+					<?php if($this->session->flashdata('login_error')): ?>	
+			<div class="alert dark alert-warning alert-dismissible" id="infoMessage"><button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button><?php echo $this->session->flashdata('login_error');?></div>
+			<?php endif; ?>	
+            <?php if($this->session->flashdata('forsuccess')): ?>  
+            <div class="alert dark alert-success alert-dismissible" id="infoMessage"><button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button><?php echo $this->session->flashdata('forsuccess');?></div>
+            <?php endif; ?>	
+                    <form id="loginform" name="loginform" method="post" action="<?php echo base_url('customer/inventery_loginpost');?>" class="form-horizontal" role="form">
+                        <div class=" form-group">
+                            <label class="control-label">Email</label>
+                            <input type="text" class="form-control" id="email" name="email"  placeholder="Email">
+                        </div>
+
+                        <div  class=" form-group">
+                           <label class="control-label">Password</label>
+                            <input id="password" type="password" class="form-control" name="password" placeholder="password">
+                        </div>
+                        
+                        <div class="">                          
+							<div class="pull-right">
+                                <label>
+                                    <a href="<?php echo base_url('customer/inve_forgotpassword');?>" >Forgot Password?</a>
+                                </label>
+                            </div>
+                        </div>
+                        <div style="margin-top:10px" class="form-group">
+                            <!-- Button -->
+                            <div class="col-sm-12 controls">
+                                <button class="btn btn-lg btn-block signup-btn" style="background-color:#c33c12;color:#fff;" type="submit">
+                                    login</button>
+
+                            </div>
+                        </div>
+				</form>
+                </div>
+            </div>
+            </div>
+            </div>
+        </div>
+		<a  href="<?php echo base_url('customer/inve'); ?>" class="" style="position: absolute;top:-100px;right:43%">
+			<img src="<?php echo base_url(); ?>assets/images/inv_logo.png" />
+		</a>
+    </div>
+
+</body>
+
+</html>
+	<script type="text/javascript">
+
+$(document).ready(function() {
+    $('#loginform').bootstrapValidator({
+       
+        fields: {
+            
+           
+			
+			email: {
+             validators: {
+					notEmpty: {
+						message: 'Email is required'
+					},
+					regexp: {
+					regexp: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+					message: 'Please enter a valid email address. For example johndoe@domain.com.'
+					}
+				}
+            },
+			
+			password: {
+					validators: {
+					notEmpty: {
+						message: 'Password is required'
+					},
+					stringLength: {
+                        min: 6,
+                        message: 'Password  must be greater than 6 characters'
+                    },
+					regexp: {
+					regexp:/^[ A-Za-z0-9_@.,/!;:}{@#&`~'"\\|=^?$%*)(_+-]*$/,
+					message: 'Password wont allow <>[]'
+					}
+				}
+			}
+        }
+    });
+});
+</script>
