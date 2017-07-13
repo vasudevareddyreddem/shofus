6105e74f9078b8677f2ab30b2a337fc61f83dc70
vasu <vasudevareddy549@gmail.com>
2017-07-13 16:17:26 +0530

    date purpose
    date purpose

--- application/views/customer/inventry/seller_payments.php
+++ application/views/customer/inventry/seller_payments.php
@@ -1,14 +1,19 @@
-<div class="content-wrapper">
+<div class="content-wrapper pad_t100">
     <!-- Content Header (Page header) -->
       <div class="container">
          <!-- Main content -->
       <div class="row">
-      <div class="box">
-            <div class="box-header">
-              <h3 class="box-title">Seller Payments</h3>
+	  <?php //echo '<pre>';print_r($seller_order_items);exit; ?>
+      <div class="box data_box_wid">
+            <div class="box-header" style="border-bottom:1px solid #ddd;">
+              <h3 class="box-title" >Seller Payment List</h3>
             </div>
             <!-- /.box-header -->
-            <div class="box-body">
+            <div class="box-body"><?php if($this->session->flashdata('success')): ?>
+					<div class="alert dark alert-success alert-dismissible" id="infoMessage"><button type="button" class="close" data-dismiss="alert" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+					</button><?php echo $this->session->flashdata('success');?></div>	
+					<?php endif; ?>
               <table id="example1" class="table table-bordered table-striped">
                 <thead>
                 <tr>
@@ -26,7 +31,7 @@
                 <?php  
                   foreach($seller_payment as $payment) {?>
                 <tr>                  
-                  <td><?php echo $payment['seller_rand_id']; ?></td>
+                  <td><a href="<?php echo base_url('inventory/sellerpaymentdetails/'.base64_encode($payment['seller_id'])); ?>"><?php echo $payment['seller_rand_id']; ?></a></td>
                   <td><?php echo $payment['seller_name']; ?></td>
                   <td><a href="<?php echo base_url('inventory/sellerpaymentdetails/'.base64_encode($payment['seller_id'])); ?>"><?php echo $payment['orderscount']; ?></a></td>
                   <td><?php echo $payment['totalamount']; ?></td>
@@ -37,7 +42,7 @@
                  <?php }?>
                 </tbody>              
               </table>
-            
+             
             </div>
             <!-- /.box-body -->
           </div>
@@ -45,16 +50,3 @@
 </div>
 </div>
 </div>
-<script>
-  $(function () {
-    $("#example1").DataTable();
-    $('#example2').DataTable({
-      "paging": true,
-      "lengthChange": false,
-      "searching": false,
-      "ordering": true,
-      "info": true,
-      "autoWidth": false
-    });
-  });
-</script>
