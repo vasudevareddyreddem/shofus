80f5512280da8ef184958753e90a7ca9e6448e08
bayapucg <vasudevareddy549@gmail.com>
2017-07-13 16:25:26 +0530

    chat page
    admin notification page

--- application/views/customer/inventry/adminnotificationview.php
+++ application/views/customer/inventry/adminnotificationview.php
@@ -1,57 +1,137 @@
-<style>
-	.detail_ta th{
-		width:40%;
-	}
-	.detail_ta {
-		background-color:#fff;
-	}
-</style>
 <div class="content-wrapper" style="padding-top:80px;">
+	 <div class="col-md-8 col-md-offset-2">
+              <!-- DIRECT CHAT -->
+              <div class="box box-primary direct-chat direct-chat-primary">
+                <div class="box-header with-border">
+                  <h3 class="box-title"> Chat</h3>
 
-    <!-- Content Header (Page header) -->
-      <div class="container">
-         <!-- Main content -->
-      <div class="row">
-		<div class="col-md-6 well detail_ta col-md-offset-3">
-			<div class="pull-left"><h3 style="padding-bottom:10px;margin:0;color:#c33c12;">SubCategory Details</h3></div>
-			<div class="pull-right"><a href="<?php echo base_url('inventory/subcategorieslist'); ?>" type="button" class="btn btn-warning btn-xs">Back</a></div>
-			 <table class="table table-bordered">
-					<tbody>
-					  <tr>
-						<th >SubCategory Name</th>
-						<td><?php echo isset($subcategory_details['subcategory_name'])?$subcategory_details['subcategory_name']:''; ?></td>
-						
-					  </tr> 
-					  <tr>
-						<th >Category Name</th>
-						<td><?php echo isset($subcategory_details['category_name'])?$subcategory_details['category_name']:''; ?></td>
-						
-					  </tr> 
-					  <tr>
-						<th >Created date</th>
-						<td><?php echo isset($subcategory_details['created_at'])?$subcategory_details['created_at']:''; ?></td>
-						
-					  </tr> 
-					  <tr>
-						<th>Status</th>
-						<td><?php if($subcategory_details['status']==1){ echo "Active";}else{ echo "Deactive";} ?></td>
-						
-					  </tr>
-					   <tr>
-						<th >Created By</th>
-						<td><?php echo isset($subcategory_details['cust_firstname'])?$subcategory_details['cust_firstname']:''; ?>&nbsp;<?php echo isset($subcategory_details['cust_lastname'])?$subcategory_details['cust_lastname']:''; ?></td>
-						
-					  </tr> 
-					
-					 
-					
-					</tbody>
-			</table> 
-			<div class="pull-right"><a href="<?php echo base_url('inventory/subcategoryedit/'.base64_encode($subcategory_details['subcategory_id'])); ?>" type="button" class="btn btn-warning btn-xs">Edit</a></div>
-			
-		</div>
-     
+                  <div class="box-tools pull-right">
+                    <span data-toggle="tooltip" title="3 New Messages" class="badge bg-yellow">3</span>
+                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                    </button>
+                    <button type="button" class="btn btn-box-tool" data-toggle="tooltip" title="Contacts" data-widget="chat-pane-toggle">
+                      <i class="fa fa-comments"></i></button>
+                    <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i>
+                    </button>
+                  </div>
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body">
+                  <!-- Conversations are loaded here -->
+                  <div class="direct-chat-messages">
+                    <!-- Message. Default to the left -->
+                    <div class="direct-chat-msg">
+                      <div class="direct-chat-info clearfix">
+                        <span class="direct-chat-name pull-left">Bayapureddy</span>
+                        <span class="direct-chat-timestamp pull-right">23 Jan 2:00 pm</span>
+                      </div>
+                      <!-- /.direct-chat-info -->
+                      <img class="direct-chat-img" src="https://lh3.googleusercontent.com/-kMEBuGZ4uhQ/AAAAAAAAAAI/AAAAAAAAAAA/AI6yGXxn6Hg1jK9kYf_HvY91K3CaSNTDVA/s32-c-mo/photo.jpg" alt="message user image"><!-- /.direct-chat-img -->
+                      <div class="direct-chat-text">
+                        Is this template really for free? That's unbelievable!
+                      </div>
+                      <!-- /.direct-chat-text -->
+                    </div>
+                    <!-- /.direct-chat-msg -->
 
-	  </div>
+                    <!-- Message to the right -->
+                    <div class="direct-chat-msg right">
+                      <div class="direct-chat-info clearfix">
+                        <span class="direct-chat-name pull-right">vasu deva</span>
+                        <span class="direct-chat-timestamp pull-left">23 Jan 2:05 pm</span>
+                      </div>
+                      <!-- /.direct-chat-info -->
+                      <img class="direct-chat-img" src="https://lh5.googleusercontent.com/-5ZIgyZg9UwQ/AAAAAAAAAAI/AAAAAAAACe4/Lo7IJZ56_EY/s32-c-k-no/photo.jpg" alt="message user image"><!-- /.direct-chat-img -->
+                      <div class="direct-chat-text">
+                        You better believe it!
+                      </div>
+                      <!-- /.direct-chat-text -->
+                    </div>
+                    <!-- /.direct-chat-msg -->
+
+                    <!-- Message. Default to the left -->
+                    <div class="direct-chat-msg">
+                      <div class="direct-chat-info clearfix">
+                        <span class="direct-chat-name pull-left">Bayapureddy</span>
+                        <span class="direct-chat-timestamp pull-right">23 Jan 5:37 pm</span>
+                      </div>
+                      <!-- /.direct-chat-info -->
+                      <img class="direct-chat-img" src="https://lh3.googleusercontent.com/-kMEBuGZ4uhQ/AAAAAAAAAAI/AAAAAAAAAAA/AI6yGXxn6Hg1jK9kYf_HvY91K3CaSNTDVA/s32-c-mo/photo.jpg" alt="message user image"><!-- /.direct-chat-img -->
+                      <div class="direct-chat-text">
+                        Working with AdminLTE on a great new app! Wanna join?
+                      </div>
+                      <!-- /.direct-chat-text -->
+                    </div>
+                    <!-- /.direct-chat-msg -->
+
+                    <!-- Message to the right -->
+                    <div class="direct-chat-msg right">
+                      <div class="direct-chat-info clearfix">
+                        <span class="direct-chat-name pull-right">vasu deva</span>
+                        <span class="direct-chat-timestamp pull-left">23 Jan 6:10 pm</span>
+                      </div>
+                      <!-- /.direct-chat-info -->
+                      <img class="direct-chat-img" src="https://lh5.googleusercontent.com/-5ZIgyZg9UwQ/AAAAAAAAAAI/AAAAAAAACe4/Lo7IJZ56_EY/s32-c-k-no/photo.jpg" alt="message user image"><!-- /.direct-chat-img -->
+                      <div class="direct-chat-text">
+                        I would love to.
+                      </div>
+                      <!-- /.direct-chat-text -->
+                    </div>
+					 <div class="direct-chat-msg">
+                      <div class="direct-chat-info clearfix">
+                        <span class="direct-chat-name pull-left">Bayapureddy</span>
+                        <span class="direct-chat-timestamp pull-right">23 Jan 5:37 pm</span>
+                      </div>
+                      <!-- /.direct-chat-info -->
+                      <img class="direct-chat-img" src="https://lh3.googleusercontent.com/-kMEBuGZ4uhQ/AAAAAAAAAAI/AAAAAAAAAAA/AI6yGXxn6Hg1jK9kYf_HvY91K3CaSNTDVA/s32-c-mo/photo.jpg" alt="message user image"><!-- /.direct-chat-img -->
+                      <div class="direct-chat-text">
+                        Working with AdminLTE on a great new app! Wanna join?
+                      </div>
+                      <!-- /.direct-chat-text -->
+                    </div>
+                    <!-- /.direct-chat-msg -->
+
+                    <!-- Message to the right -->
+                    <div class="direct-chat-msg right">
+                      <div class="direct-chat-info clearfix">
+                        <span class="direct-chat-name pull-right">vasu deva</span>
+                        <span class="direct-chat-timestamp pull-left">23 Jan 6:10 pm</span>
+                      </div>
+                      <!-- /.direct-chat-info -->
+                      <img class="direct-chat-img" src="https://lh5.googleusercontent.com/-5ZIgyZg9UwQ/AAAAAAAAAAI/AAAAAAAACe4/Lo7IJZ56_EY/s32-c-k-no/photo.jpg" alt="message user image"><!-- /.direct-chat-img -->
+                      <div class="direct-chat-text">
+                        I would love to.
+                      </div>
+                      <!-- /.direct-chat-text -->
+                    </div>
+                    <!-- /.direct-chat-msg -->
+
+                  </div>
+            
+
+                 
+                  <!-- /.direct-chat-pane -->
+                </div>
+                <!-- /.box-body -->
+                <div class="box-footer">
+                  <form action="#" method="post">
+                    <div class="input-group">
+                      <input type="text" name="message" placeholder="Type Message ..." class="form-control">
+                          <span class="input-group-btn">
+                            <button type="button" class="btn btn-primary btn-flat">Send</button>
+                          </span>
+                    </div>
+                  </form>
+                </div>
+                <!-- /.box-footer-->
+              </div>
+              <!--/.direct-chat -->
+			  <br>
+
+            </div>
+			<div class="clearfix"><br>
+<br>
+<br></div>
+
 </div>
-</div>
+
