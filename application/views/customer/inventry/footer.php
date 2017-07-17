9b11cd27c9e75bd029e50222e52ec356c97e60cf
bayapucg <vasudevareddy549@gmail.com>
2017-07-17 13:18:15 +0530

    inventary
    links changes

--- application/views/customer/inventry/footer.php
+++ application/views/customer/inventry/footer.php
@@ -52,7 +52,8 @@
 
 
 <!--home page product scroller start here --> 
-
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/home/js/common.js"></script> 
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/home/js/owl.carousel.min.js"></script> 
 
 
 
