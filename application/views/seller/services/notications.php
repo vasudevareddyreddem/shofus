16891199003b905efbba9b5cfd9631db95abff77
vasudevareddyreddem <vasudevareddy549@gmail.com>
2017-07-21 15:16:17 +0530

    chating purpose
    chating purpose

--- application/views/seller/services/notications.php
+++ application/views/seller/services/notications.php
@@ -657,7 +657,27 @@
                     <!-- Message. Default to the left -->
 				  <?php foreach ($notificationlist as $notification){ ?>
 				<?php if($notification['message_type']=='REPLIED'){ ?>
-                      <div class="direct-chat-msg">
+                      <div class="direct-chat-msg right">
+                      <div class="direct-chat-info clearfix">
+                        <span class="direct-chat-name pull-right"><?php echo isset($notification['cust_firstname'])?$notification['cust_firstname']:''; ?>&nbsp;<?php echo isset($notification['cust_lastname'])?$notification['cust_lastname']:''; ?></span>
+                        <span class="direct-chat-timestamp pull-left"><?php echo date('M j h:i A',strtotime(htmlentities($notification['created_at'])));?></span>
+                      </div>
+                      <!-- /.direct-chat-info -->
+						<?php if($notification['cust_propic']!=''){ ?>
+					   <img class="direct-chat-img" src="<?php echo base_url('uploads/profile/'.$notification['cust_propic']); ?>" alt="<?php echo $notification['cust_propic']; ?>"><!-- /.direct-chat-img -->
+						<?php }else{ ?>
+					   <img class="direct-chat-img" src="<?php echo base_url('uploads/profile/'); ?>/default.jpg" alt="Logo"><!-- /.direct-chat-img -->
+						<?php } ?>  
+						<div class="direct-chat-text">
+                        <?php echo $notification['seller_message']; ?>
+                      </div>
+                    </div>
+					<?php } ?>
+					
+					
+					<?php if($notification['message_type']=='REPLY'){ ?>
+					
+					 <div class="direct-chat-msg">
                       <div class="direct-chat-info clearfix">
                         <span class="direct-chat-name pull-left"><?php echo isset($notification['seller_name'])?$notification['seller_name']:''; ?> </span>
                         <span class="direct-chat-timestamp pull-right"><?php echo date('M j h:i A',strtotime(htmlentities($notification['created_at'])));?></span>
@@ -670,26 +690,6 @@
 
 					  <?php } ?>
                       <div class="direct-chat-text">
-                        <?php echo $notification['seller_message']; ?>
-                      </div>
-                    </div>
-					<?php } ?>
-					
-					
-					<?php if($notification['message_type']=='REPLY'){ ?>
-					
-					<div class="direct-chat-msg right">
-                      <div class="direct-chat-info clearfix">
-                        <span class="direct-chat-name pull-right"><?php echo isset($notification['cust_firstname'])?$notification['cust_firstname']:''; ?>&nbsp;<?php echo isset($notification['cust_lastname'])?$notification['cust_lastname']:''; ?></span>
-                        <span class="direct-chat-timestamp pull-left"><?php echo date('M j h:i A',strtotime(htmlentities($notification['created_at'])));?></span>
-                      </div>
-                      <!-- /.direct-chat-info -->
-						<?php if($notification['cust_propic']!=''){ ?>
-					   <img class="direct-chat-img" src="<?php echo base_url('uploads/profile/'.$notification['cust_propic']); ?>" alt="<?php echo $notification['cust_propic']; ?>"><!-- /.direct-chat-img -->
-						<?php }else{ ?>
-					   <img class="direct-chat-img" src="<?php echo base_url('uploads/profile/'); ?>/default.jpg" alt="Logo"><!-- /.direct-chat-img -->
-						<?php } ?>  
-						<div class="direct-chat-text">
                         <?php echo $notification['seller_message']; ?>
                       </div>
                     </div>
