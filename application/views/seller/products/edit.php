d0a3e9732dcbe03df0309aa27a002d59f5b6d596
raghava yaddanapudi <vasudareddy549@gmail.com>
2017-07-24 17:45:30 +0530

    edit product
    add row

--- application/views/seller/products/edit.php
+++ application/views/seller/products/edit.php
@@ -228,7 +228,7 @@
 													<div class="col-md-6" style="padding:0">
 														<input style="border-radius:0px 5px 5px 0px" type="text" placeholder="Specification Value"  class="form-control" id="specificationvalueid" name="specificationvalue[]" value="" >
 													</div>
-													
+													<div id="addrs1"></div>
 												</div>
 												
 												<div class="pull-right" id="delbtn" style="padding-top:10px;display:none">
@@ -500,7 +500,7 @@ $(document).ready(function(){
      $("#add_sep").click(function(){
       $('#addrs'+k).html("<div class='col-md-6' style='padding:0'><input style='border-radius:5px 0px 0px 5px' type='text' class='form-control' id='specificationnameid[]' name='specificationname[]' ></div><div class='col-md-6' style='padding:0'><input style='border-radius:0px 5px 5px 0px' type='text' class='form-control' id='specificationvalueid[]' name='specificationvalue[]' ></div>");
 		$('#tab_sep').append('<div id="addrs'+(k+1)+'"></div>');
-		if(k >=2){
+		if(k >=1){
 			$('#delbtn').show();
 		}
 	  k++; 
