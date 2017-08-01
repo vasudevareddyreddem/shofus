1da4569bc4da897a87f3d16b8823695b361d00a0
bayapucg <vasudevareddy549@gmail.com>
2017-08-01 15:52:27 +0530

    customer portal
    changes design

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -52,10 +52,12 @@
             <div id="top-categories" class="product-flexslider hidden-buttons">
               <div class="slider-items slider-width-col4 products-grid">
         <?php foreach ($topoffers as $tops){  ?>
-        <div class="item">
-                  <div class="pro-img"><img src="<?php echo base_url('uploads/products/'.$tops['item_image']); ?>" alt="<?php echo $tops['item_name']; ?>">
-                    <div class="pro-info"><?php echo $tops['item_name']; ?></div>
+        <div class="item" style="border: 1px #ddd solid;">
+                  <div class="pro-img img-wrapper  img_hover"><img src="<?php echo base_url('uploads/products/'.$tops['item_image']); ?>" alt="<?php echo $tops['item_name']; ?>">
+                    
           </div>
+		  
+		  <div class="pro-info" style="border-top:1px solid #ddd;"><?php echo $tops['item_name']; ?></div>
                   </div>
               <?php } ?>
         </div>
