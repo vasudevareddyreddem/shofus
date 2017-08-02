dfa012d2e0816439e48157ee74ff2c3fec156b06
raghava yaddanapudi <vasudareddy549@gmail.com>
2017-08-02 17:52:22 +0530

    inventory portal changes
    product quantity

---
+++ application/views/customer/inventry/categorywise_quantity.php
@@ -0,0 +1,53 @@
+<div class="content-wrapper pad_t100">
+    <!-- Content Header (Page header) -->
+      <div class="container">
+         <!-- Main content -->
+      <div class="row">
+      <div class="box data_box_wid">
+            <div class="box-header" style="border-bottom:1px solid #ddd;">
+              <h3 class="box-title" >Category wise Quantity</h3>
+              <a class="pull-right btn btn-sm btn-primary" href="<?php echo base_url('inventory/productquantity'); ?>" class="box-title">Back</a>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              <table id="example1" class="table table-bordered table-striped">
+                <thead>
+				  <tr>
+					<th>Seller id</th>
+					<th>Seller Name</th>
+          <th>Category Name</th>
+          <th>Subcategory Name</th>
+          <th>Item Name</th>
+					<th>Total Quantity</th>
+					
+				 </tr>
+                </thead>
+                <tbody>
+                <?php  
+                  foreach($category_wise as $category) {?>
+                <tr>  
+                  <td><?php echo $category['seller_rand_id'] ?></td>
+                  <td><?php echo $category['seller_name'] ?></td>
+                  <td><?php echo $category['category_name'] ?></td>
+                  <td><?php echo $category['subcategory_name'] ?></td>
+                  <td><?php echo $category['item_name'] ?></td>
+                  <td><?php echo $category['item_quantity'] ?></td>                  
+                </tr>
+                 <?php }?>
+                </tbody>              
+              </table>
+             
+            </div>
+            <!-- /.box-body -->
+          </div>
+
+</div>
+</div>
+</div>
+<script>
+$(document).ready(function() {
+    $('#example1').DataTable( {
+        "order": [[ 2, "desc" ]]
+    } );
+} );
+</script>
