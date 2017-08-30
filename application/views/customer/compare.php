e6ee3fc655dec834d322fc156d6db2fb2e8d2c87
vasudevareddyreddem <vasudevareddy549@gmail.com>
2017-08-30 15:43:22 +0530

    comapre products changes
    comapre products changes

--- application/views/customer/compare.php
+++ application/views/customer/compare.php
@@ -86,147 +86,85 @@
                     </li>
                 </ul>
             </div>
-        </div> <div class="col-md-4 text-center">
-            <div class="panel panel-default panel-pricing">
-                <div class="panel-heading">
-                    <img class="img-responsive" src="<?php echo base_url('uploads/products/'.$compore_products['item_image']); ?>">                   
-                </div>
-
-                <ul class="list-group text-center">
-                    <li class="list-group-item">Product Name:
-                        <?php echo $compore_products[ 'item_name'];?>
-                    </li>
-                    <li class="list-group-item">Product Price:
-                        <?php echo ($compore_products[ 'item_cost']); ?>
-                    </li>
-                    <li class="list-group-item">Product Code:
-                        <?php echo $compore_products[ 'item_code']; ?>
-                    </li>
-                </ul>
-            </div>
-        </div> <div class="col-md-4 text-center">
-            <div class="panel panel-default panel-pricing">
-                <div class="panel-heading">
-                    <img class="img-responsive" src="<?php echo base_url('uploads/products/'.$compore_products['item_image']); ?>">                   
-                </div>
-
-                <ul class="list-group text-center">
-                    <li class="list-group-item">Product Name:
-                        <?php echo $compore_products[ 'item_name'];?>
-                    </li>
-                    <li class="list-group-item">Product Price:
-                        <?php echo ($compore_products[ 'item_cost']); ?>
-                    </li>
-                    <li class="list-group-item">Product Code:
-                        <?php echo $compore_products[ 'item_code']; ?>
-                    </li>
-                </ul>
+        </div>
+		<div id="compare_items" class="col-md-4 text-center" style="display:none;"></div>
+		<div id="compare_items2" class="col-md-4 text-center" style="display:none;"></div>
+		
+		<div class="col-md-3 text-center" id="item_hide1">
+            <div class="form-group nopaddingRight col-md-6 san-lg" >
+                <label for="exampleInputPassword1">Select Compare Product</label>
+                <select class="form-control" onchange="productitem_one(this.value);" id="item_id" name="item_id">
+                    <option>Select item</option>
+                    <?php foreach($item as $item_data){ ?>
+                    <option value="<?php echo $item_data->item_id; ?>">
+                        <?php echo $item_data->item_name; ?></option>
+                    <?php } ?>
+                </select>
             </div>
         </div>
+		<div class="col-md-3 text-center" id="item_hide2">
+            <div class="form-group nopaddingRight col-md-6 san-lg" >
+                <label for="exampleInputPassword1">Select Compare Product</label>
+                <select class="form-control" onchange="productitem_two(this.value);" id="item_idtwo" name="item_idtwo">
+                    <option>Select item</option>
+                    <?php foreach($item as $item_data){ ?>
+                    <option value="<?php echo $item_data->item_id; ?>">
+                        <?php echo $item_data->item_name; ?></option>
+                    <?php } ?>
+                </select>
+            </div>
+        </div>
+		
         
 
     </div>
 </div>
 <input type="hidden" name="category_id" id="category_id" value="<?php echo $compore_products['category_id']; ?>">      
-    <!--  <div class="row">
-	  <div class="col-md-12">
-			<h4>Product Details</h4>
-	  </div>
-	  <div class="col-md-12">
-		<div class="col-md-3">
-			<table class="table table-bordered table-compare">
-				<tr>
-					<th>Product Name</th>
-				</tr>
-				<tr>
-					<th style="width: 40%">Product Image</th>
-				</tr>
-				<tr>
-					<th>Product Price</th>
-				</tr>
-        <tr>
-          <th>Product code</th>
-        </tr>
-			</table>
-		</div>
-			<div class="col-md-3">
-			<table class="table table-bordered table-compare">
-				<tr>
-					<td><?php echo $compore_products['item_name'];?></td>
-          <td id="compare_item_name"></td>
-				</tr>
-				<tr>
-					<td><img class="img-responsive" style="width: 40%" src="<?php echo base_url('uploads/products/'.$compore_products['item_image']); ?>"></td>
-				</tr>
-				<tr>
-					<td><?php echo ($compore_products['item_cost']); ?></td>
-				</tr>
-        <tr>
-          <td><?php echo $compore_products['item_code']; ?></td>
-        </tr>
-			</table>
-			</div>
-       <input type="hidden" name="category_id" id="category_id"  value="<?php echo $compore_products['category_id']; ?>">
-			<div class="col-md-3" id="item_hide">
-			<div class="form-group nopaddingRight col-md-6 san-lg">
-                  <label for="exampleInputPassword1">Select Item</label>
-                  <select class="form-control" id="item_id" name="item_id">
-                      <option>Select item</option> 
-                        <?php foreach($item as $item_data){ ?>
-                      <option value="<?php echo $item_data->item_id; ?>"><?php echo $item_data->item_name; ?></option>
-                    <?php } ?> 
-                  </select>
-                </div>
-			</div>
-      <!-- <div class="col-md-3" id="compare_items" ></div> -->
-          <nav aria-label="Shopping Cart Next Navigation">
-            <ul class="pager">
-              <li class="previous"><a href="products.html"><span aria-hidden="true">&larr;</span> Continue Shopping</a></li>
-              <li class="next"><a href="cart.html">My Shopping Cart <span aria-hidden="true">&rarr;</span></a></li>
-            </ul>
-          </nav>
+   
          
     
 
 <script type="text/javascript">
-    $(document).ready(function()
-    {
-    $("#item_id").change(function()
-    {
-    var id=$(this).val();
-    //alert(id);
-    //var dataString = 'item_id='+ id;
-    var item_id = $("#item_id").val();
-    var category_id = $("#category_id").val();
-    //alert(category_id);exit;
-    //var item_name = $("#item_id").val();
-    //alert(item_id);exit;
-    $.ajax
-    ({
-    type: "POST",
-    url: "<?php echo base_url();?>category/compare_items_list",
-     data: {item_id:item_id,category_id:category_id},
-     cache: false,
-    dataType:'html',
-    success: function (data) {
-
-       $('#compare_items').append(data);
-       //$('#compare_items')..css("display", "block");
-       $('#item_hide').hide();
-     } 
-    });
     
-    });
-    });
-
-//     var count =0;
-// $('#btnTimeextend').on('click',function(){
-//     $('#item_hide').show();
-//   count = count +1;
-//   if(count == 3){
-//   $('#btnTimeextend').hide();
-//   }
-// })
-
-
-    </script>   
+    function productitem_one(id)
+    {
+		var category_id = $("#category_id").val();
+		$.ajax({
+				type: "POST",
+				url: "<?php echo base_url();?>category/compare_items_list",
+				data: {
+					item_id:id,
+					category_id:category_id
+					},
+				cache: false,
+				dataType:'html',
+				success: function (data) {
+					$('#compare_items').show();
+					$('#compare_items').append(data);
+					$('#item_hide1').hide();
+				} 
+			});	
+    
+    }
+	function productitem_two(id)
+    {
+		var category_id = $("#category_id").val();
+		$.ajax({
+				type: "POST",
+				url: "<?php echo base_url();?>category/compare_items_list",
+				data: {
+					item_id:id,
+					category_id:category_id
+					},
+				cache: false,
+				dataType:'html',
+				success: function (data) {
+					$('#compare_items2').show();
+					$('#compare_items2').append(data);
+					$('#item_hide2').hide();
+				} 
+			});	
+    
+    }
+  
+ </script>   
--- application/views/customer/compareone.php
+++ application/views/customer/compareone.php
@@ -1,121 +1,23 @@
-<!-- 
-<div class="row">
-			<div class="col-md-3">
-			<table class="table table-bordered table-compare">
-				<tr>
-					<td><?php echo $compare_one['item_name'];?></td>
-				</tr>
-				<tr>
-					<td><img class="img-responsive" style="width: 40%" src="<?php echo base_url('uploads/products/'.$compare_one['item_image']); ?>"></td>
-				</tr>
-				<tr>
-					<td><?php echo ($compare_one['item_cost']); ?></td>
-				</tr>
-        <tr>
-          <td><?php echo $compare_one['item_code']; ?></td>
-        </tr>
-			</table>
-			</div>
-			<div class="col-md-3" id="compare_items_two" ></div>
-      
-			<div class="col-md-3" id="item_hide_one">
-			<div class="form-group nopaddingRight col-md-6 san-lg">
-                  <label for="exampleInputPassword1">Select One Item</label>
-                  <select class="form-control" id="item_id_one" name="item_id_one">
-                      <option>Select item</option> 
-                        <?php foreach($items as $item_data){ ?>
-                      <option value="<?php echo $item_data->item_id; ?>"><?php echo $item_data->item_name; ?></option>
-                    <?php } ?> 
-                  </select>
-                </div>
-			</div>
-	  </div> -->
+ <div class="panel panel-default panel-pricing">
+		<div class="panel-heading">
+			<img class="img-responsive" src="<?php echo base_url('uploads/products/'.$compare_one['item_image']); ?>">
+		</div>
+		<ul class="list-group text-center">
+			<li class="list-group-item">Product Name:
+				<?php echo $compare_one[ 'item_name'];?>
+			</li>
+			<li class="list-group-item">Product Price:
+				<?php echo $compare_one[ 'item_cost']; ?>
+			</li>
+			<li class="list-group-item">Product Code:
+				<?php echo $compare_one[ 'item_code']; ?>
+			</li>
 
-  <div class="container">
-    <div class="row">
-        <div class="col-md-4 text-center" id="div_hide">
-            <div class="panel panel-default panel-pricing">
-                <div class="panel-heading">
-                    <img class="img-responsive" src="<?php echo base_url('uploads/products/'.$compare_one['item_image']); ?>">
-                   <!-- <button id="close_product">Close</button>  -->
-                </div>
-                <ul class="list-group text-center">
-                    <li class="list-group-item">Product Name:
-                        <?php echo $compare_one[ 'item_name'];?>
-                    </li>
-                    <li class="list-group-item">Product Price:
-                        <?php echo $compare_one[ 'item_cost']; ?>
-                    </li>
-                    <li class="list-group-item">Product Code:
-                        <?php echo $compare_one[ 'item_code']; ?>
-                    </li>
+		</ul>
 
-                </ul>
-
-            </div>
-        </div>
-<!-- <button id="btnTimeextend_one">ADD</button> -->        
-        <div class="col-md-3" id="item_hide_one">
-            <div class="form-group nopaddingRight col-md-6 san-lg">
-                <label for="exampleInputPassword1">Select Compare Productsss</label>
-                <select class="form-control" id="item_id_one" name="item_id_one">
-                    <option>Select item</option>
-                    <?php foreach($items as $item_data){ ?>
-                    <option value="<?php echo $item_data->item_id; ?>">
-                        <?php echo $item_data->item_name; ?></option>
-                    <?php } ?>
-                </select>
-            </div>
-        </div>
-        <div class="col-md-3" id="compare_items_two"></div>
-    </div>
 </div>
+     
 
-	  <script type="text/javascript">
-    $(document).ready(function()
-    {
-    $("#item_id_one").change(function()
-    {
-    var id=$(this).val();
-    //alert(id);
-    //var dataString = 'item_id='+ id;
-    var item_id = $("#item_id_one").val();
-      
-    $.ajax
-    ({
-    type: "POST",
-    url: "<?php echo base_url();?>category/compare_items_list_two",
-     data: {item_id:item_id},
-     cache: false,
-    dataType:'html',
-    success: function (data) {
 
-       $('#compare_items_two').append(data);
-       $('#item_hide_one').hide();
-     } 
-    });
-    
-    });
-    });
 
-//  var count =0;
-// $('#close_product').on('change',function(){
-//     $('#item_hide_one').show();
-//     $('#div_hide').hide();
-//     count = count +1;
-//   if(count == 3){
-//   $('#item_hide_one').hide();
-//   }
-
- 
-// })
-
-// var myEl = document.getElementById('close_product');
-
-// myEl.addEventListener('click', function() {
-//   //$('#item_hide_one').show();
-//     $('#hide_div').hide();
-    
-// }, false);
-
-    </script> 
+	
