7e6067d0d666260a62f145f043a97221c436684a
bayapucg <vasudevareddy549@gmail.com>
2017-09-05 11:07:01 +0530

    whish
    design

--- application/views/customer/wishlist.php
+++ application/views/customer/wishlist.php
@@ -1,94 +1,14 @@
-
-<style>
-.panel-title > a:before {
-    float: left !important;
-    font-family: FontAwesome;
-    content:"\f1db";
-    padding-right: 5px;
-}
-
-.panel-title > a:hover, 
-.panel-title > a:active, 
-.panel-title > a:focus  {
-    text-decoration:none;
-}
-
-.stepwizard-step p {
-    margin-top: 10px;    
-}
-
-.stepwizard-row {
-    display: table-row;
-}
-
-.stepwizard {
-    display: table;     
-    width: 100%;
-    position: relative;
-}
-
-.stepwizard-step button[disabled] {
-    opacity: 1 !important;
-    filter: alpha(opacity=100) !important;
-}
-
-.stepwizard-row:before {
-    top: 14px;
-    bottom: 0;
-    position: absolute;
-    content: " ";
-    width: 100%;
-    height: 1px;
-    background-color: #ccc;
-    z-order: 0;
-    
-}
-
-.stepwizard-step {    
-    display: table-cell;
-    text-align: center;
-    position: relative;
-}
-
-.btn-circle {
-  width: 30px;
-  height: 30px;
-  text-align: center;
-  padding: 6px 0;
-  font-size: 12px;
-  line-height: 1.428571429;
-  border-radius: 15px;
-}
-.font_span{
-	font-size:17px;
-}
-.table>tbody>tr>td, .table>tbody>tr>th, .table>tfoot>tr>td, .table>tfoot>tr>th, .table>thead>tr>td, .table>thead>tr>th{
-	border:none;
-}
-tr th:first-child,
-tr th:last-child {
-    width:40%;
-	font-weight:400;
-	color:#aaa;
-}
-</style>
-
-
 <body >
 <div class="container">
 		<div class="row">
-		<?php //echo '<pre>';print_r($whistlist_items);exit; ?>
-		<div class="container">
-		<div class="panel panel-primary">
-			<div class="panel-heading ">Wishlist</div>
-			<div class="panel-body">
+	
+		
 			<section>
-        <div class="">
+       
            
-                <div class="tab-content">
-                    <div class="tab-pane active" role="tabpanel" id="step1">
+             
 					 
-          <div class="table-responsive">
+          <div class="table-responsive panel col-md-offs">
 		  <?php if($this->session->flashdata('adderror')): ?>
 			<div class="alert dark alert-warning alert-dismissible" id="infoMessage"><button type="button" class="close" data-dismiss="alert" aria-label="Close">
                       <span aria-hidden="true">&times;</span>
@@ -99,7 +19,7 @@ tr th:last-child {
                       <span aria-hidden="true">&times;</span>
                     </button><?php echo $this->session->flashdata('productsuccess');?></div>
 			<?php endif; ?>
-            <table class="table table-bordered table-cart">
+            <table class="table ">
               <thead>
                 <tr>
                   <th>Product</th>
@@ -118,9 +38,9 @@ tr th:last-child {
 				<input type="hidden" name="wishlist" id="wishlist" value="1" >
          
 			    <tr>
-                  <td class="img-cart">
+                  <td class="">
                     <a href="<?php echo base_url('category/productview/'.base64_encode($items['item_id'])); ?>">
-                      <img src="<?php echo base_url('uploads/products/'.$items['item_image']); ?>" class="img-thumbnail">
+                      <img style="width:150px;" src="<?php echo base_url('uploads/products/'.$items['item_image']); ?>" class="img-thumbnail">
                     </a>
                   </td>
 				  <td>
@@ -181,17 +101,12 @@ tr th:last-child {
          
 						<div class="clearfix"></div>
           
-                    </div>
-                
-                   
-                
-                </div>
+               
           
-        </div>
+       
     </section>
-	   </div>
-	   </div>
-	   </div>
+	  
+	 
 	   
 	   </div>
 	 <!-- track start-->
