50c9c1f48272b889d9185a8fad421b241ba172c6
vasudevareddyreddem <vasudevareddy549@gmail.com>
2017-09-08 18:12:52 +0530

    search functionality purpose
    search functionality purpose

--- application/views/customer/search.php
+++ application/views/customer/search.php
@@ -1,11 +1,12 @@
 <?php
 if(count($detail)>0){
-foreach($detail as $searhitems){  
+foreach($detail as $searhitems){
+//echo '<pre>';print_r($searhitems);exit;	
  if($searhitems['yes']==0){  ?>
 	 
-	<a  style="text-decoration:none;" href="<?php echo base_url('category/subcategoryview/'.base64_encode($searhitems['category_id'])); ?>"><li style="list-style-type:none;"><?php  echo ucfirst($searhitems['subcategory_name']);  ?></li></a> 
+	<a  style="text-decoration:none;" href="<?php echo base_url('category/subcategoryview/'.base64_encode($searhitems['category_id'])); ?>"><li style="list-style-type:none;"><?php  echo ucfirst($searhitems['subcategory_name']);  ?> in <?php echo ucfirst($searhitems['category_name']); ?></li></a> 
   <?php }else{ ?>
-	 <a  style="text-decoration:none;" href="<?php echo base_url('category/productview/'.base64_encode($searhitems['item_id'])); ?>"><li style="list-style-type:none;"><?php  echo ucfirst($searhitems['item_name']);  ?></li> </a>
+	 <a  style="text-decoration:none;" href="<?php echo base_url('category/productview/'.base64_encode($searhitems['item_id'])); ?>"><li style="list-style-type:none;"><?php  echo ucfirst($searhitems['item_name']);  ?> in <?php echo ucfirst($searhitems['subcategory_name']); ?></li> </a>
 
 	 
 <?php  } } }else{ ?>
