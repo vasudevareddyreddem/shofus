e0fe1bc315fc0c4fbae6650b4f85c2a3bc60ee34
vasudevareddyreddem <vasudevareddy549@gmail.com>
2017-09-11 18:41:51 +0530

    compare products l in hide and show purpose
    compare products l in hide and show purpose

--- application/views/customer/compareone.php
+++ application/views/customer/compareone.php
@@ -1,8 +1,8 @@
  
  <div class="panel panel-default panel-pricing">
- 
+ <?php //echo '<pre>';print_r($compare_one);?>
 		<div class="panel-heading">
-		<i style="position:absolute;right:36px;top:0;font-size:18px;background:#fff;border-radius:50%;padding:5px;" onclick="removefunction('compare_items');" class="fa fa-times" aria-hidden="true"></i>
+		<i style="position:absolute;right:36px;top:0;font-size:18px;background:#fff;border-radius:50%;padding:5px;" onclick="removefunction('compare_items','<?php echo $compare_one['item_id']; ?>');" class="fa fa-times" aria-hidden="true"></i>
 			<div class="clearfix"></div>
 			<a href="<?php echo base_url('category/productview/'.base64_encode($compare_one['item_id']));?>">
 			<img class="img-responsive" src="<?php echo base_url('uploads/products/'.$compare_one['item_image']); ?>">
@@ -26,10 +26,11 @@
 </div>
 
 <script>
-function removefunction(id){
+function removefunction(id,itemid){
 	$('#'+id).hide();
 	$('#item_hide1').show();
 	$("#item_id").val('');
+	$("#"+itemid).prop("disabled", false);
 	document.getElementById("item_id").selectedIndex = 0;
 
 	
--- application/views/customer/comparetwo.php
+++ application/views/customer/comparetwo.php
@@ -2,7 +2,7 @@
  <div class="panel panel-default panel-pricing">
  
 		<div class="panel-heading">
-		<i style="position:absolute;right:36px;top:0;font-size:18px;background:#fff;border-radius:50%;padding:5px;" onclick="removefunction2('compare_items2');" class="fa fa-times" aria-hidden="true"></i>
+		<i style="position:absolute;right:36px;top:0;font-size:18px;background:#fff;border-radius:50%;padding:5px;" onclick="removefunction2('compare_items2','<?php echo $compare_one['item_id']; ?>');" class="fa fa-times" aria-hidden="true"></i>
 			<div class="clearfix"></div>
 			<a href="<?php echo base_url('category/productview/'.base64_encode($compare_one['item_id']));?>">
 			<img class="img-responsive" src="<?php echo base_url('uploads/products/'.$compare_one['item_image']); ?>">
@@ -26,10 +26,11 @@
 </div>
 
 <script>
-function removefunction2(id){
+function removefunction2(id,itemid){
 	$('#'+id).hide();
 	$('#item_hide2').show();
 	$("#item_idtwo").val('');
+	$("#"+itemid).prop("disabled", false);
 	document.getElementById("item_idtwo").selectedIndex = 0;
 	
 }
