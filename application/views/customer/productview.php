02f62d96794541392fdd6c8fe5f77324cfd87406
vasudevareddyreddem <vasudevareddy549@gmail.com>
2017-10-05 10:50:13 +0530

    product view page details changes purpose
    product view page details changes purpose

--- application/views/customer/productview.php
+++ application/views/customer/productview.php
@@ -51,15 +51,13 @@
     padding: 0px;
     background: #fff;
 }
-.table>tbody>tr>td, .table>tbody>tr>th, .table>tfoot>tr>td, .table>tfoot>tr>th, .table>thead>tr>td, .table>thead>tr>th{
-	border-top:none;
-}
+
 </style>
 
 <div class="pad_bod" style="margin:0;padding:0; " >
 		<div class="" >
 		
-		<div class="col-md-3 z_ind " ">
+		<div class="col-md-3 z_ind ">
 			<ul id="glasscase" class="gc-start">
                     <li>
 						<img class="img-responsive" src="<?php echo base_url('uploads/products/'.$products_list['item_image']); ?>"/>
@@ -346,8 +344,9 @@
           <!-- Nav tabs -->
           <ul class="nav nav-tabs" role="tablist">
             <li role="presentation" class="active"><a href="#desc" aria-controls="desc" role="tab" data-toggle="tab">Description</a></li>
-			<li role="presentation" class=""><a href="#desc" aria-controls="desc" role="tab" data-toggle="tab">Specifications</a></li>
-            <li role="presentation"><a href="#detail" aria-controls="detail" role="tab" data-toggle="tab">Detail</a></li>
+			<li role="presentation" class=""><a href="#specification" aria-controls="desc" role="tab" data-toggle="tab">Specifications</a></li>
+            <li role="presentation"><a href="#warnty" aria-controls="detail" role="tab" data-toggle="tab">Warranty Details</a></li>
+            <li role="presentation"><a href="#returnpolices" aria-controls="detail" role="tab" data-toggle="tab">Return Policy</a></li>
             <li role="presentation"><a href="#review" aria-controls="review" role="tab" data-toggle="tab">Reviews (<?php echo count($products_reviews); ?>)</a></li>
           </ul>
           <!-- End Nav tabs -->
@@ -359,16 +358,13 @@
               <div role="tabpanel" class="tab-pane active" id="desc">
                  <div class="well">
                   <p>
-                  <?php echo $products_list['item_description']; ?> </p>
+                  <?php echo $products_list['description']; ?> </p>
                 </div>
-              </div>
-              <!-- End Description Tab Content -->
-
-              <!-- Detail Tab Content -->
-              <div role="tabpanel" class="tab-pane" id="detail">
-                <div class="well">
-                  <table class="table table-bordered">
-                    <tbody>
+              </div> 
+			  <div role="tabpanel" class="tab-pane" id="specification">
+                 <div class="well">
+				 <table class="table table-bordered">
+                <tbody>
                       <tr>
                         <td>Item Name</td>
                         <td><?php echo $products_list['item_name']; ?></td>
@@ -394,82 +390,85 @@
                       </tr>
                       <tr>
                         <td>Item Code</td>
-                        <td><?php echo $products_list['item_code']; ?></td>
+                        <td><?php echo $products_list['product_code']; ?></td>
                       </tr>
-					  <?php if(isset($products_list['ideal_for']) && $products_list['ideal_for']!=''){ ?>
-					   <tr>
-                        <td>Ideal for</td>
-                        <td><?php echo $products_list['ideal_for']; ?></td>
-                      </tr>
-					  <?php } ?>
-					  <?php if(isset($products_list['item_sub_name']) && $products_list['item_sub_name']!=''){ ?>
-					   <tr>
-                        <td>Item Sub name</td>
-                        <td><?php echo $products_list['item_sub_name']; ?></td>
-                      </tr>
-					  <?php } ?>
-					  <?php if(isset($products_list['sufficient_for']) && $products_list['sufficient_for']!=''){ ?>
-					   <tr>
-                        <td>Sufficient for</td>
-                        <td><?php echo $products_list['sufficient_for']; ?></td>
-                      </tr>
-					  <?php } ?>
-					  <?php if(isset($products_list['producttype']) && $products_list['producttype']!=''){ ?>
-					   <tr>
-                        <td>Type</td>
-                        <td><?php echo $products_list['producttype']; ?></td>
-                      </tr>
-					  <?php } ?>
-					  <?php if(isset($products_list['theme']) && $products_list['theme']!=''){ ?>
-					   <tr>
-                        <td>Theme</td>
-                        <td><?php echo $products_list['theme']; ?></td>
-                      </tr>
-					  <?php } ?>
-					  <?php if(isset($products_list['dial_shape']) && $products_list['dial_shape']!=''){ ?>
-					   <tr>
-                        <td>Dial Shape</td>
-                        <td><?php echo $products_list['dial_shape']; ?></td>
-                      </tr>
-					  <?php } ?>
-					  <?php if(isset($products_list['compatibleos']) && $products_list['compatibleos']!=''){ ?>
-					   <tr>
-                        <td>Compatible mobiles</td>
-                        <td><?php echo $products_list['compatibleos']; ?></td>
-                      </tr>
-					  <?php } ?>
-					  <?php if(isset($products_list['usage']) && $products_list['usage']!=''){ ?>
-					   <tr>
-                        <td>Usage</td>
-                        <td><?php echo $products_list['usage']; ?></td>
-                      </tr>
-					  <?php } ?>
-					  <?php if(isset($products_list['display_type']) && $products_list['display_type']!=''){ ?>
-					   <tr>
-                        <td>Display Type</td>
-                        <td><?php echo $products_list['display_type']; ?></td>
-                      </tr>
-					  <?php } ?>
-					  <?php if(isset($products_list['occasion']) && $products_list['occasion']!=''){ ?>
-					   <tr>
-                        <td>Occasion</td>
-                        <td><?php echo $products_list['occasion']; ?></td>
-                      </tr>
-					  <?php } ?>
-					  <?php if(isset($products_list['material']) && $products_list['material']!=''){ ?>
-					   <tr>
-                        <td>Material</td>
-                        <td><?php echo $products_list['material']; ?></td>
-                      </tr>
-					  <?php } ?> 
-					  <?php if(isset($products_list['gemstones']) && $products_list['gemstones']!=''){ ?>
-					   <tr>
-                        <td>Material</td>
-                        <td><?php echo $products_list['gemstones']; ?></td>
-                      </tr>
-					  <?php } ?>
+					  
                     </tbody>
                   </table>
+                </div>
+              </div>
+              <!-- End Description Tab Content -->
+
+              <!-- Detail Tab Content -->
+              <div role="tabpanel" class="tab-pane" id="warnty">
+                <div class="well">
+                 <table class="table table-bordered">
+                <tbody>
+                      <tr>
+                        <td>Item Name</td>
+                        <td><?php echo $products_list['item_name']; ?></td>
+                      </tr>
+                      <tr>
+                        <td>Item Cost</td>
+                        <?php if(date('m/d/Y') <= $products_list['offer_expairdate'] && date('H:ia')<= $products_list['offer_time']) {?>
+                        	<td><?php echo ($products_list['item_cost'])-($products_list['offer_amount']); ?></td>
+                        	<?php }else{ ?>
+                        		<td><?php echo $products_list['item_cost']; ?></td>
+                        		<?php }?>
+                      </tr>
+                      <tr>
+                        <td>Item Status</td>
+							<td>
+							<?php if($products_list['item_status']==1){ 
+							echo "Ready Stock";
+							}
+							else{
+							echo "Out of Stock";
+							}				?>
+							</td>
+                      </tr>
+                      <tr>
+                        <td>Item Code</td>
+                        <td><?php echo $products_list['product_code']; ?></td>
+                      </tr>
+					  
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+			  <div role="tabpanel" class="tab-pane" id="returnpolices">
+                <div class="well">
+                  <table class="table table-bordered">
+                <tbody>
+                      <tr>
+                        <td>Item Name</td>
+                        <td><?php echo $products_list['item_name']; ?></td>
+                      </tr>
+                      <tr>
+                        <td>Item Cost</td>
+                        <?php if(date('m/d/Y') <= $products_list['offer_expairdate'] && date('H:ia')<= $products_list['offer_time']) {?>
+                        	<td><?php echo ($products_list['item_cost'])-($products_list['offer_amount']); ?></td>
+                        	<?php }else{ ?>
+                        		<td><?php echo $products_list['item_cost']; ?></td>
+                        		<?php }?>
+                      </tr>
+                      <tr>
+                        <td>Item Status</td>
+							<td>
+							<?php if($products_list['item_status']==1){ 
+							echo "Ready Stock";
+							}
+							else{
+							echo "Out of Stock";
+							}				?>
+							</td>
+                      </tr>
+                      <tr>
+                        <td>Item Code</td>
+                        <td><?php echo $products_list['product_code']; ?></td>
+                      </tr>
+					  
+                    </tbody>
                   </table>
                 </div>
               </div>
