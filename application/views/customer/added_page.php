6b9532f99d3cec0c2acd1eacd8c82c50606181fc
bayapucg <vasudevareddy549@gmail.com>
2017-12-14 15:56:50 +0530

    added
    changes

--- application/views/customer/added_page.php
+++ application/views/customer/added_page.php
@@ -125,3 +125,69 @@
       
       </div>
     </section>
+	<section>
+	<div class="best-pro slider-items-products container_main">
+		<div class="row mar_t15" >
+			<div class="col-md-3">
+				<img class="img-responsive" src="https://rukminim1.flixcart.com/flap/960/960/image/5bafa3.jpg?q=50">
+			</div>
+			<div class="col-md-3">
+				<img class="img-responsive" src="https://rukminim1.flixcart.com/flap/960/960/image/de54b5.jpg?q=50">
+			</div>
+			<div class="col-md-3">
+				<img class="img-responsive" src="https://rukminim1.flixcart.com/flap/960/960/image/5bafa3.jpg?q=50">
+			</div>
+			<div class="col-md-3">
+				<img class="img-responsive" src="https://rukminim1.flixcart.com/flap/960/960/image/de54b5.jpg?q=50">
+			</div>
+			
+				
+		</div>
+	</div>
+		
+    </section>
+	<section class="">
+      <div class=" slider-items-products container_main">
+       
+		   <div id="best-seller" class="product-flexslider hidden-buttons cat_sl_s" >
+				  
+			<div class="slider-items slider-width-col4 products-grid  text-center" >
+				<a href="#">
+				<div class="item cat_ma" >
+				  <div class=" box-product-outer" >
+					<div class="box-product">
+					   <img src="<?php echo base_url('assets/home/images/fruit.png'); ?>" > 
+					<h5 class="text-center">Sample test</h5>
+					</div>
+					
+				  </div>
+				</div>
+				</a>
+				<a href="#">
+				<div class="item cat_ma" >
+				  <div class=" box-product-outer" >
+					<div class="box-product">
+					   <img src="<?php echo base_url('assets/home/images/fruit.png'); ?>" > 
+					<h5 class="text-center">Sample test</h5>
+					</div>
+					
+				  </div>
+				</div>
+				</a><a href="#">
+				<div class="item cat_ma" >
+				  <div class=" box-product-outer" >
+					<div class="box-product">
+					   <img src="<?php echo base_url('assets/home/images/fruit.png'); ?>" > 
+					<h5 class="text-center">Sample test</h5>
+					</div>
+					
+				  </div>
+				</div>
+				</a>
+			</div>
+
+		   </div>
+	
+      
+      </div>
+    </section>
